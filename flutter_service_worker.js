d0dfbe975a9781bd019d87669af4edc8e32c039a
Abdelwahab <aaaaaaaaaab743@gmail.com>
2025-07-27 20:08:48 +0300

    Fixing phone errors

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "a0f0441885330ae89e0f1cdeecd60f9d",
+const RESOURCES = {"assets/Abdelwahab%2520Mohamed%2520Abdelwahab%2520-%2520Flutter%2520Developer%2520CV.pdf": "8356ea6b066ff765e25a45d937f9eec4",
+"assets/AssetManifest.bin": "a0f0441885330ae89e0f1cdeecd60f9d",
 "assets/AssetManifest.bin.json": "0b9d7433176234b2ca20cc0dc5d37c34",
 "assets/AssetManifest.json": "04572dcca5a9425264eff50d697ef93e",
 "assets/assets/Abdelwahab%2520Mohamed%2520Abdelwahab%2520-%2520Flutter%2520Developer%2520CV.pdf": "8356ea6b066ff765e25a45d937f9eec4",
@@ -38,14 +39,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "a0f0441885330ae89e0f1cdeecd60f9d
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "fa17c276e2899b38652a21cb1e2fae7e",
+"flutter_bootstrap.js": "8a02a3c95eb99c1daaf77fcd766807d9",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "d27acd450f8cf6aa964284a934ba07f9",
 "/": "d27acd450f8cf6aa964284a934ba07f9",
-"main.dart.js": "e43f6649851d01994fff4a40248f15af",
+"main.dart.js": "805173ae946ae1d9102a7f68e2a9e5d5",
 "manifest.json": "ab435ccfcedbeefa2518e79b583ba5f1",
 "version.json": "75cc6ad459c847f6f0fb554da1aff717"};
 // The application shell files that are downloaded before a service worker can
